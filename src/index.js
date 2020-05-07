1dc6379602c8ad4ca9ad2f676520d10cea4f9e08
kumhoe.lau <nerojz86@gmail.com>
2020-05-07 22:16:25 +0800

    refact: Create a DetailComment component
    Refact the comment html into a component.
    Import Faker library to generate random avatar image.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="ui container comments"></div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
