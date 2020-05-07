2f57fd989feabd2dd3da42a8e4210b50132b8bd8
kumhoe.lau <nerojz86@gmail.com>
2020-05-07 22:45:41 +0800

    feat: Add properties to DetailComponent
    Define properties on the component to pass custom data.
    Use props. to access the data.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-  return <div className="ui container comments"></div>;
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45pm"
+        content="Nice blog"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Sophia"
+        timeAgo="Today at 2:00am"
+        content="Great! The insight is valuable!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Joe"
+        timeAgo="Yesterday at 5:45pm"
+        content="Awesome!"
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
